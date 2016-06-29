5381cbe70f72245f1394d13a5d14f6a4ee8e7b37
brijeshgautam <brijesh.bhu@gmail.com>
2016-06-29 19:00:07 +0000

    Committing map-reduce implementation for  UFO data analysis

---
+++ StreamImplementation/mapper_shape_time.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import re
+import sys
+
+pattern = re.compile('(\d+)\s*(min|sec|hour)', re.I)
+
+#line = 'abcd   123 SECODS'
+
+for line in sys.stdin:
+    line = line.split('\t')
+    if len(line[3]) > 0 and len(line[4]) > 0:
+         matched = pattern.search(line[4])
+         if matched != None:
+              value = int(matched.group(1))
+              unit = matched.group(2) 
+              if re.match(unit, 'min', re.I)!=  None :
+                  value = value * 60 
+              elif re.match(unit, 'hour', re.I)!=  None :
+                  value = value * 3600 
+              print line[3], '\t' ,value
---
+++ StreamImplementation/reducer_shape_time.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import re
+import sys
+
+current = None
+min = 0
+max = 0
+mean = 0
+total = 0
+count = 0
+
+for line in sys.stdin:
+    line = line.split('\t')
+    shape = line[0]
+    time = int(line[1])
+    if current == None :
+       current = shape 
+       min= time
+       max = time
+       total = time
+       count =1 
+    elif current == shape :
+       count += 1 
+       total + time 
+       if time < min:
+           min = time 
+       elif max < time :
+           max = time 
+    else:
+       print shape ,'\t',min,'\t',max,'\t',total/float(count)
+       current = shape 
+       min = time 
+       max = time 
+       total = time 
+       count = 1 
+    
+if current != None :
+     print shape ,'\t',min,'\t',max,'\t',total/float(count)
+   
