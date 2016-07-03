2557500844f7cea7c0a83893477e95305dc41775
brijeshgautam <brijesh.bhu@gmail.com>
2016-07-03 12:08:26 +0000

    Implementation  of UFO shape  and observed time analysis  in Java

---
+++ JavaImplementation/UFOAnalysisDriverClass.java
@@ -0,0 +1,39 @@
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * Created by ANIMESH on 02-07-2016.
+ */
+public class UFOAnalysisDriverClass {
+
+    public static void main(String[] args) throws Exception
+    {
+
+        Job job = Job.getInstance();
+        Configuration config = new Configuration(false) ;
+        ChainMapper.addMapper(job, UFORecordValidationMapper.class, LongWritable.class,
+                Text.class, LongWritable.class , Text.class, config);
+
+        Configuration extractTimeConf= new Configuration(false);
+
+        ChainMapper.addMapper( job,  MapExtractTimeClass.class, LongWritable.class, Text.class,
+                Text.class, LongWritable.class,  extractTimeConf) ;
+
+        job.setMapperClass(ChainMapper.class);
+        job.setJarByClass(UFOAnalysisDriverClass.class);
+        job.setReducerClass(GetMinMaxMeanReducer.class);
+        FileInputFormat.setInputPaths(job,new Path(args[0])) ;
+        FileOutputFormat.setOutputPath(job, new Path(args[1])) ;
+        System.exit(job.waitForCompletion(true)?0:1);
+
+    }
+
+}
