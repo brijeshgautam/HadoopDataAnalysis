240c8ee747f1533c57ba3b5bece497ff3edb9e91
root <root@sandbox.hortonworks.com>
2016-08-25 16:20:13 +0000

    Updated to use ToolRunner

--- JavaImplementation/PatentData/InvertPatentRecord.java
+++ JavaImplementation/PatentData/InvertPatentRecord.java
@@ -1,73 +1,75 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.*;
 import  org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
+
 import java.util.Iterator;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.log4j.Logger;
-import org.apache.hadoop.mapreduce.Counter;
+
 
 /**
  *  * Created by bgautam on 8/18/2016.
  *   */
 
 
-public class InvertPatentRecord {
- public     enum LogCounter {TOTAL_RECORD, TOTAL_REDUCE };
-
-public  static class MapPatentRecord extends Mapper<Text, Text, Text, Text> {
+class MapPatentRecord extends Mapper<Text, Text, Text, Text> {
 
     public void map(Text key , Text value, Context context) throws IOException, InterruptedException
     {
-           context.write(value, key);
-           Counter counter = context.getCounter(LogCounter.class.getName(), LogCounter.TOTAL_RECORD.toString());
-	  counter.increment(1);
+        context.write(value, key);
     }
- }
+}
 
-public static class ReducePatentRecord extends Reducer < Text, Text, Text, Text > {
-
+class ReducePatentRecord extends Reducer < Text, Text, Text, Text > {
     public void reduce (Text key, Iterable<Text>values , Context context) throws IOException, InterruptedException
     {
         String csv = "";
-        Iterator<Text> itr = values.iterator();
+        Iterator <Text> itr = values.iterator();
         while (itr.hasNext()){
             if (csv.length() > 0){
                 csv += ",";
             }
             csv += itr.next().toString();
         }
-        Counter counter = context.getCounter(LogCounter.class.getName(), LogCounter.TOTAL_REDUCE.toString());
-        counter.increment(1);
         context.write(key, new Text(csv));
     }
 }
-      public static  void main(String [] args) throws  IOException, InterruptedException, ClassNotFoundException{
-        Configuration conf = new Configuration();
+public class InvertPatentRecord extends Configured implements Tool {
+
+    public int run (String [] args)throws Exception{
+        Configuration conf = getConf();
         conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator",",");
         Job job = Job.getInstance(conf, "invertpatentrecord");
         job.setJarByClass(InvertPatentRecord.class);
         job.setMapperClass(MapPatentRecord.class);
-        job.setReducerClass(ReducePatentRecord.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        Path  outputPath = new Path(args[1]);
+        FileSystem fout = FileSystem.get(conf);
+        Path outPath = new Path(args[1]);
+        if(fout.exists(outPath))
+            fout.delete(outPath, true);
         FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, outputPath);
-        FileSystem fs = FileSystem.get(conf);
-        if(fs.exists(outputPath)){
-               fs.delete(outputPath,true);
-        }
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setReducerClass(ReducePatentRecord.class);
         job.setInputFormatClass(KeyValueTextInputFormat.class);
         job.setJobName("Invert Patent Record");
-        boolean rc =job.waitForCompletion(true);
-        System.exit(rc == true ? 0 : 1);
+        System.exit(job.waitForCompletion(true)? 0: 1);
+        return 0;
+
+    }
+
+    public static  void main(String [] args) throws  Exception, InterruptedException, ClassNotFoundException{
+        int res = ToolRunner.run(new Configuration(),new InvertPatentRecord(),args);
+
+        System.exit(res);
     }
 }
+
